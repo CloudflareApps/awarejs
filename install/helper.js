474bb4012d28c488a80db5b76efce0e0a73cd038
Zack Bloom <zackbloom@gmail.com>
2015-01-15 02:19:05 -0500

    Always show message while previewing

--- install/helper.js
+++ install/helper.js
@@ -1,11 +1,15 @@
 window.EagerAwareHelper = function(options){
   var element = Eager.createElement(options.location);
 
-  if (element && window.reader[options.when] && options.message){
-    var message = document.createElement('div');
-    message.className = 'eager-aware-message';
-    message.innerHTML = options.message;
+  var isPreview = window.Eager && window.Eager.installs && window.Eager.installs.preview && window.Eager.installs.preview.appId === 'TWjTnHVeNK0v';
 
-    element.appendChild(message);
+  if (element && options.message && options.when){
+    if (window.reader[options.when] || isPreview){
+      var message = document.createElement('div');
+      message.className = 'eager-aware-message';
+      message.innerHTML = options.message;
+
+      element.appendChild(message);
+    }
   }
 }
