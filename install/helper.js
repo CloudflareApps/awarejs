dc86bf056824d5f0c83501965acc2db218c040f4
Zack Bloom <zackbloom@gmail.com>
2015-01-15 01:53:36 -0500

    Don't show undefined message

--- install/helper.js
+++ install/helper.js
@@ -1,7 +1,7 @@
 window.EagerAwareHelper = function(options){
   var element = Eager.createElement(options.location);
 
-  if (element && window.reader[options.when]){
+  if (element && window.reader[options.when] && options.message){
     var message = document.createElement('div');
     message.className = 'eager-aware-message';
     message.innerHTML = options.message;
