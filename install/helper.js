f09a0c18a7ac5c001ecdce9271968216c8bf8823
Zack Bloom <zackbloom@gmail.com>
2015-01-15 01:51:16 -0500

    Add css

--- install/helper.js
+++ install/helper.js
@@ -2,6 +2,10 @@ window.EagerAwareHelper = function(options){
   var element = Eager.createElement(options.location);
 
   if (element && window.reader[options.when]){
-    element.innerHTML = options.message;
+    var message = document.createElement('div');
+    message.className = 'eager-aware-message';
+    message.innerHTML = options.message;
+
+    element.appendChild(message);
   }
 }
