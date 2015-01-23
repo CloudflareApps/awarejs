e9db43add364e070c8184629f635df7eb86cced6
Adam Schwartz <aschwartz@hubspot.com>
2015-01-22 23:26:16 -0600

    Styles and improved install options

--- install/helper.js
+++ install/helper.js
@@ -1,14 +1,21 @@
-window.EagerAwareHelper = function(options){
+window.EagerAwareHelper = function(options) {
   var element = Eager.createElement(options.location);
 
   var isPreview = window.Eager && window.Eager.installs && window.Eager.installs.preview && window.Eager.installs.preview.appId === 'TWjTnHVeNK0v';
 
-  if (element && options.message && options.when){
-    if (window.reader[options.when] || isPreview){
+  if (element && options.message && options.when) {
+    if (window.reader[options.when] || isPreview) {
       var message = document.createElement('div');
       message.className = 'eager-aware-message';
-      message.innerHTML = options.message;
+      message.style.fontSize = options.style.fontSize;
 
+      var content = document.createElement('div');
+      content.className = 'eager-aware-message-content';
+      content.style.color = options.style.color;
+      content.style.backgroundColor = options.style.backgroundColor;
+      content.innerHTML = options.message;
+
+      message.appendChild(content);
       element.appendChild(message);
     }
   }
